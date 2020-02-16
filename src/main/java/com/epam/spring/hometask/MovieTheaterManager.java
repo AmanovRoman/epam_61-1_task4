4d835ce7acdd618ae5a39e6134246d3ae0824b7f
AmanovRoman <amanov.roman@gmail.com>
2020-02-16 23:43:51 +0400

    configs

---
+++ src/main/java/com/epam/spring/hometask/MovieTheaterManager.java
@@ -0,0 +1,14 @@
+package com.epam.spring.hometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheaterManager {
+    public MovieTheaterManager() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTheaterManager.class, args);
+    }
+}
---
+++ src/main/java/com/epam/spring/hometask/config/CommonConfiguration.java
@@ -0,0 +1,36 @@
+package com.epam.spring.hometask.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan({"com.epam.spring.hometask"})
+@PropertySource({"classpath:database.properties"})
+@EnableAspectJAutoProxy
+public class CommonConfiguration {
+    @Autowired
+    Environment environment;
+
+    public CommonConfiguration() {
+    }
+
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(this.environment.getRequiredProperty("database.driver"));
+        dataSource.setUrl(this.environment.getRequiredProperty("database.url"));
+        dataSource.setUsername(this.environment.getRequiredProperty("database.user"));
+        dataSource.setPassword(this.environment.getRequiredProperty("database.password"));
+        return dataSource;
+    }
+
+    @Bean
+    SimpleJdbcInsert simpleJdbcInsert(@Autowired DataSource dataSource) {
+        return new SimpleJdbcInsert(dataSource);
+    }
+}
